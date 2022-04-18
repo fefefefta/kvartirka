83759860ff775cd79bced15bb373af5102241a56
fefta <stuffefta@gmail.com>
2022-04-18 13:55:46 +0300

    serializers done

---
+++ app/blog/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Article, Comment
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'text']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'article', 'answered_to', 'text']
