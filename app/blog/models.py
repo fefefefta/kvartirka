573217d5d33818b6fbdb323a8d003e732282e5f4
fefta <stuffefta@gmail.com>
2022-04-18 13:09:44 +0300

    article and comment models

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,22 @@
+from mptt.models import MPTTModel, TreeForeignKey
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField(blank=True)
+
+
+class Comment(MPTTModel):
+    answered_to = TreeForeignKey(
+            'self',
+            on_delete=models.DO_NOTHING,
+            null=True,
+            blank=True,
+        )
+    article = models.ForeignKey(
+            'Article',
+            on_delete=models.CASCADE,
+            blank=False,
+        )
+    text = models.TextField(blank=True)
